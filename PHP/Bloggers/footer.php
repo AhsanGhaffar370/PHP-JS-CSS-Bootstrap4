569ce7f5e12119ccd9b8cbce61e2f1fc8d47d28f
AhsanRao <35622245+Ahsanrao237@users.noreply.github.com>
2021-04-01 10:57:51 +0500

    starting ajax from scratch

--- PHP/Bloggers/footer.php
+++ PHP/Bloggers/footer.php
@@ -1,151 +1,23 @@
-<footer class="decor text-left footer_gray_color" >
-
-    <div class="footer_black p-3">
-    <div class="container text-center text-lg-right">
-        <a href="https://www.facebook.com/RethinkingLegal/" class="btn btn-lg p-2"><i class="fab fa-facebook-f fa-2x foot1"></i></a>
-                
-        <a href="https://www.facebook.com/RethinkingLegal/" class="btn btn-lg p-2 "><i class="fab fa-twitter fa-2x foot1"></i></a>
-        
-        <a href="https://www.facebook.com/RethinkingLegal/" class="btn btn-lg  p-2"><i class="fab fa-instagram fa-2x foot1"></i></a>
-        
-        <a href="https://www.youtube.com/channel/UCcfi8y3cb3Ja9YfuNZbNLKA" class="btn btn-lg  p-2"><i class="fab fa-youtube fa-2x foot1"></i></a>
-    </div>
-    </div>
-
-
-    <div class="footer_gray">
-    <div class="container pt-5 pb-4">
-
-        <div class="row pt-3 pb-2">
-
-
-        
-
-            <div class="col-12 col-lg-3 col-md-6 col-sm-12 mt-4 mt-lg-0">
-
-                <p class="size15 font-weight-bold text-white">CONTACT US</p>
-
-                <p class="size14 font-weight-light">
-                    <span class="font-weight-bold">Address:</span>  2182 Hewitt Ave
-Kettering, Ohio 45440
-                </p>
-                <p class="size14 font-weight-bold">
-                    Email:<a href="mailto: info@affordablelegalhelp.com" class="foot1 pl-2">info@affordablelegalhelp.com</a> 
-                </p>
-                <p class="size14 font-weight-bold">
-                   Phone: 844 944 5342
-                </p>
-
-            </div>
-
-
-
-
-            <div class="col-12 col-lg-3 col-md-6 col-sm-12 pl-2 pl-lg-4 pl-md-4 mt-4 mt-lg-0">
-
-                <p class="size15 font-weight-bold pl-2 text-white">OUR SERVICES</p>
-
-                <ul class="list-group list-group-flush m-0 p-0">
-
-                    <li class="list-group-item footer_gray border-0 pt-0 pl-3 pb-2"> 
-                        <a href="/divorce" class="size15 foot1 footer_gray_color">Divorce</a>
-                    </li>
-                    <li class="list-group-item footer_gray border-secondary pl-3 pt-3 pb-2"> 
-                        <a href="/child-support" class="size15 foot1 footer_gray_color">Child Support</a>
-                    </li>
-                    <li class="list-group-item footer_gray border-secondary pl-3 pt-3 pb-2"> 
-                        <a href="/child-addoption" class="size15 foot1 footer_gray_color">Child Adoption</a>
-                    </li>
-                    <li class="list-group-item footer_gray border-secondary pl-3 pt-3 pb-2"> 
-                        <a href="/child-custody" class="size15 foot1 footer_gray_color">Child Custody</a>
-                    </li>
-                    <li class="list-group-item footer_gray border-secondary border-bottom pl-3 pt-3 pb-2"> 
-                        <a href="/grand-parent-rights" class="size15 foot1 footer_gray_color">Grand Parent Right's</a>
-                    </li>
-                </ul>
-
-            </div>
-
-
-
-
-
-            <div class="col-12 col-lg-3 col-md-6 col-sm-12 mt-5 mt-lg-0">
-
-                <p class="size15 font-weight-bold text-white">FOLLOW US</p>
-                
-                 <a href="https://www.facebook.com/RethinkingLegal/" class="btn btn-lg p-2"><i class="fab fa-facebook-f fa-2x foot1"></i></a>
-                
-        <a href="https://www.facebook.com/RethinkingLegal/" class="btn btn-lg p-2 "><i class="fab fa-twitter fa-2x foot1"></i></a>
-        
-        <a href="https://www.facebook.com/RethinkingLegal/" class="btn btn-lg  p-2"><i class="fab fa-instagram fa-2x foot1"></i></a>
-                
-                <a href="https://www.youtube.com/channel/UCcfi8y3cb3Ja9YfuNZbNLKA" class="btn btn-lg  p-2"><i class="fab fa-youtube fa-2x foot1"></i></a>
-                <!-- <ul class="list-group list-group-flush m-0 p-0">
-
-                    <li class="list-group-item footer_gray border-0 pl-3 pt-3 pb-2"> 
-                        <a href="about" class="foot1">Facebook</a>
-                    </li>
-                    <li class="list-group-item footer_gray border-secondary pl-3 pt-3 pb-2"> 
-                        <a href="about" class="foot1">Twitter</a>
-                    </li>
-                    <li class="list-group-item footer_gray border-secondary pl-3 pt-3 pb-2"> 
-                        <a href="about" class="foot1">Instagram</a>
-                    </li>
-                    <li class="list-group-item footer_gray border-secondary pl-3 pt-3 pb-2"> 
-                        <a href="about" class="foot1">Youtube</a>
-                    </li>
-                </ul> -->
-
-                
-
-            </div>
-
-
-
-
-            <div class="col-12 col-lg-3 col-md-6 col-sm-12 mt-5 mt-lg-0">
-
-                <p class="size15 font-weight-bold text-white">BUSINESS HOURS</p>
-
-                <p class="size14 font-weight-light">
-                Monday-Friday: 9am to 9pm <br>
-                Saturday: 10am to 4pm <br>
-                Sunday: Closed
-                </p>
-
-            </div>
-        </div>
-
-    </div>
-    </div>
-
-
-    <div class="footer_black ">
+<footer class="" >
+    <div class="footer_black">
     <div class="container">
-    <div class="row  font-weight-light pt-2 pb-2">
+    <div class="row font-weight-light pt-4 pb-4">
         
-        <div class="col-12 col-lg-7 col-md-12 col-sm-12 text-center text-lg-left d-lg-flex align-items-center">
-
-            <a href="/about-us" class="size14 foot1 pr-3">ABOUT US</a>
-            <a href="/our-blog" class="size14 foot1 pr-3"  >OUR BLOG</a>
-            <a href="/contact-us" class="size14 foot1 pr-3" >CONTACT US</a>
-
-        </div>
-
-        <div class="col-12 col-lg-5 col-md-12 col-sm-12 text-center text-lg-left mt-3 mt-lg-2">
-            <p class="size14 font-weight-light">
-                COPYRIGHT©2006-2021, ALL RIGHTS RESERVED. <br>
-                <a href="/privacy-policy" class="foot1 font-weight-normal" >PRIVACY POLICY</a> , 
-                <a href="/terms-of-use" class="foot1 font-weight-normal" >TERMS OF SERVICES</a>
-
+        <div class="col-12 col-lg-6 col-md-12 col-sm-12 text-center text-lg-left d-lg-flex align-items-center">
+            <p class="size14 text-white font-weight-light">
+            © 2020 <a href="index.php" class="foot1 font-weight-normal text-white b6" >Bloggers</a>. All Rights Reserved.
+             
             </p>
         </div>
-    </div>
-    </div>
-    </div>
 
-    
-
+        <div class="col-12 col-lg-6 col-md-12 col-sm-12 text-center text-lg-right mt-3 mt-lg-2">
+            <a href="#" class="btn btn-lg p-1"><i class="fab fa-facebook fa-1x foot1"></i></a>
+            <a href="#" class="btn btn-lg p-1"><i class="fab fa-twitter fa-1x foot1"></i></a>
+            <a href="#" class="btn btn-lg p-1"><i class="fab fa-instagram fa-1x foot1"></i></a>
+            <a href="#" class="btn btn-lg p-1"><i class="fab fa-youtube fa-1x foot1"></i></a>
+        </div>
+    </div>
+    </div>
+    </div>
 
 </footer>
